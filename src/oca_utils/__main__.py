cc4354bc55c4539912369a40aafc9c089746e516
Daniel Thonon <d.thonon9@gmail.com>
2025-04-05 22:56:20 +0200

    Suite modification paramçtres d'appel

--- src/oca_utils/__main__.py
+++ src/oca_utils/__main__.py
@@ -252,19 +252,19 @@ def convertir(
                     gx.with_suffix(".xmp_original").unlink(missing_ok=True)
 
 
-def _renommer_temp(rep_origine: Path, ctx: click.Context) -> None:
+def _renommer_temp(rep_origine: Path, dry_run: bool, force: bool) -> None:
     # Renommer en UUID, si le fichier n'est pas déjà bien nommé
     with exiftool.ExifToolHelper() as et:
         # Renommage temporaire pour éviter les écrasements
         fichiers = rep_origine.glob("*")
         for f in fichiers:
             if re.match(MEDIA_PAT, f.suffix) and (
-                ctx.obj["FORCE"] or not re.match(CORRECT_PAT, f.name)
+                force or not re.match(CORRECT_PAT, f.name)
             ):
                 mtime = f.stat().st_mtime
                 g = rep_origine / (uuid.uuid4().hex + f.suffix.lower())
                 logger.info(f"Photo/Vidéo {f.name} renommée en : {g.name}")
-                if not ctx.obj["ESSAI"]:
+                if not dry_run:
                     f.rename(g)
                     # Retour à la date originelle
                     os.utime(g, (mtime, mtime))
@@ -299,7 +299,7 @@ def _renommer_temp(rep_origine: Path, ctx: click.Context) -> None:
 
 
 def _renommer_seq_date(  # noqa: max-complexity=13
-    rep_origine: Path, ctx: click.Context
+    rep_origine: Path, dry_run: bool
 ) -> None:
     s_fichiers = []
     fichiers = rep_origine.glob("*.*")
@@ -335,7 +335,7 @@ def _renommer_seq_date(  # noqa: max-complexity=13
                 racine + fdate + "_" + f"{secrets.randbelow(10):02}" + f.suffix
             )
             logger.info(f"Photo/Vidéo {f.name}, datée {dc} à renommée en : {dest}")
-            if not ctx.obj["ESSAI"]:
+            if not dry_run:
                 mtime = f.stat().st_mtime
                 g = rep_origine / dest
                 f.rename(g)
@@ -373,17 +373,32 @@ def _renommer_seq_date(  # noqa: max-complexity=13
 
 
 @main.command()
+@click.option(
+    "--input_dir",
+    required=True,
+    type=click.Path(exists=True, dir_okay=True, readable=True),
+    help="Répertoire des fichiers AVI à convertir",
+)
+@click.option("--dry_run", is_flag=True, help="Mode test, sans renommage des fichiers.")
+@click.option(
+    "--force",
+    is_flag=True,
+    help="Force le renommage même si le nom du fichier est correct.",
+)
 @click.pass_context
-def renommer(ctx: click.Context) -> None:
-    """Renomme au format personnel les photos et vidéos."""
-    rep_origine = Path(ctx.obj["ORIGINE"])
-    logger.info(f"Renommage des photos et vidéos depuis {rep_origine}")
+def renommer(ctx: click.Context, input_dir: str, dry_run: bool, force: bool) -> None:
+    """Convertit les vidéos AVI ou  MP4 en mp4 optimisé."""
+    rep_origine = Path(input_dir).expanduser()
+    if not rep_origine.is_dir():
+        logger.fatal(f"Le répertoire d'entrée {input_dir} n'est pas valide")
+        raise FileNotFoundError
+    logger.info(f"Renommage des photos et vidéos dans {rep_origine}")
 
     # Renommage temporaire pour éviter les écrasements de fichier
-    _renommer_temp(rep_origine, ctx)
+    _renommer_temp(rep_origine, dry_run, force)
 
     # Renommage final
-    _renommer_seq_date(rep_origine, ctx)
+    _renommer_seq_date(rep_origine, dry_run)
 
 
 def noms(tags: List[str]) -> List[str]:
@@ -448,10 +463,19 @@ def corrige(sp: str) -> str:
 
 
 @main.command()
+@click.option(
+    "--input_dir",
+    required=True,
+    type=click.Path(exists=True, dir_okay=True, readable=True),
+    help="Répertoire des fichiers AVI à convertir",
+)
 @click.pass_context
-def vérifier(ctx: click.Context) -> None:  # noqa: max-complexity=13
+def vérifier(ctx: click.Context, input_dir: str) -> None:  # noqa: max-complexity=13
     """Vérification du tagging des photos et vidéos."""
-    rep_origine = Path(ctx.obj["ORIGINE"])
+    rep_origine = Path(input_dir).expanduser()
+    if not rep_origine.is_dir():
+        logger.fatal(f"Le répertoire d'entrée {input_dir} n'est pas valide")
+        raise FileNotFoundError
     logger.info(f"Vérification du tagging des photos et vidéos dans {rep_origine}")
 
     fichiers = rep_origine.glob("*.*")
@@ -533,10 +557,20 @@ def vérifier(ctx: click.Context) -> None:  # noqa: max-complexity=13
 
 
 @main.command()
+@click.option(
+    "--input_dir",
+    required=True,
+    type=click.Path(exists=True, dir_okay=True, readable=True),
+    help="Répertoire des fichiers AVI à convertir",
+)
+@click.option("--dry_run", is_flag=True, help="Mode test, sans renommage des fichiers.")
 @click.pass_context
-def géotagger(ctx: click.Context) -> None:
+def géotagger(ctx: click.Context, input_dir: str, dry_run: bool) -> None:
     """Géotagging des photos et vidéos."""
-    rep_origine = Path(ctx.obj["ORIGINE"])
+    rep_origine = Path(input_dir).expanduser()
+    if not rep_origine.is_dir():
+        logger.fatal(f"Le répertoire d'entrée {input_dir} n'est pas valide")
+        raise FileNotFoundError
     logger.info(f"Géotagging des photos et vidéos dans {rep_origine}")
 
     with open(rep_origine / "information.yaml") as info:
@@ -554,7 +588,7 @@ def géotagger(ctx: click.Context) -> None:
                 fx = Path(str(f) + ".xmp")
                 if fx.is_file():
                     logger.debug(f"Géotagging de {f.name}")
-                    if not ctx.obj["ESSAI"]:
+                    if not dry_run:
                         et.set_tags(
                             fx,
                             {
@@ -576,14 +610,38 @@ def géotagger(ctx: click.Context) -> None:
 
 
 @main.command()
+@click.option(
+    "--from_dir",
+    required=True,
+    type=click.Path(exists=True, dir_okay=True, readable=True),
+    help="Répertoire des fichiers à traiter",
+)
+@click.option(
+    "--to_dir",
+    required=False,
+    default="",
+    type=click.Path(),
+    help="Fichier CSV d'export, pour la commande exporter uniquement",
+)
+@click.option("--dry_run", is_flag=True, help="Mode test, sans renommage des fichiers.")
+@click.option(
+    "--increment", is_flag=True, help="Copie incrémentale des nouveaux médias."
+)
 @click.pass_context
-def copier(ctx: click.Context) -> None:  # noqa: max-complexity=13
+def copier(  # noqa: max-complexity=13
+    ctx: click.Context, from_dir: str, to_dir: str, increment: bool, dry_run: bool
+) -> None:
     """Copie et renomme au format OCA les photos et vidéos."""
-    rep_origine = Path(ctx.obj["ORIGINE"])
+    if not Path(from_dir).expanduser().is_dir():
+        logger.fatal(f"Le répertoire d'entrée {from_dir} n'est pas valide")
+        raise FileNotFoundError
+    rep_origine = Path(from_dir).expanduser()
     logger.info(f"Renommage des photos et vidéos depuis {rep_origine}")
-    rep_destination = Path(ctx.obj["DESTINATION"])
+    if not Path(to_dir).expanduser().is_dir():
+        logger.fatal(f"Le répertoire de destination {to_dir} n'est pas valide")
+        raise FileNotFoundError
+    rep_destination = Path(to_dir).expanduser()
     logger.info(f"Renommage des photos et vidéos vers {rep_destination}")
-    rep_destination.mkdir(exist_ok=True)
 
     # Création des chemin par date de relevé
     with open(rep_origine / "information.yaml") as info:
@@ -617,7 +675,7 @@ def copier(ctx: click.Context) -> None:  # noqa: max-complexity=13
                     parents=True, exist_ok=True
                 )
 
-    type_cp = "incrémentale" if ctx.obj["INCREMENT"] else "complète"
+    type_cp = "incrémentale" if increment else "complète"
     logger.info(f"Copie {type_cp}" + f" depuis {dernier}")
     with exiftool.ExifToolHelper() as et:
         # Détermination du nom OCA
@@ -626,7 +684,7 @@ def copier(ctx: click.Context) -> None:  # noqa: max-complexity=13
         for f in fichiers:
             if re.match(MEDIA_PAT, f.suffix):
                 date_prise = f.name.split("_")[1]
-                if not ctx.obj["INCREMENT"] or date_prise > dernier:
+                if not increment or date_prise > dernier:
                     logger.debug(f"Copie/renommage de {f.name}, daté {date_prise}")
                     # Recherche des tags de classification
                     for d in et.get_tags(f, tags=["HierarchicalSubject"]):
